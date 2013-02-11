8f95bfe66383c96406723d04462df8ed80eff507
Sarah <sjrose19@gmail.com>
2013-02-11 14:30:13 -0500

    jquery additions

---
+++ sarah.js
@@ -0,0 +1,27 @@
+$(function() {
+		$(".sarah").hover(function(){
+			$('.image > img').hide();
+			$("#sarah").show().animate({'opacity': 1});
+		});
+		$(".dog").hover(function(){	
+			$('.image > img').hide();
+			$("#bella").show().animate({'opacity': 1});
+		});
+		$(".newyork").hover(function(){	
+			$('.image > img').hide();
+			$("#newyork").show().animate({'opacity': 1});
+		});
+		$(".food").hover(function(){
+			$('.image > img').hide();
+			$('#food').show().animate({'opacity': 1})
+		})
+	});
+	$(".mainh1 > span").each(function(idx) {
+		$(this).delay( idx * 700).fadeIn(700);
+	});
+$( 'span' ).click(function( event ) {
+ console.log( 'clicked', $( this ).text() );
+});
+$( 'li' ).on( 'click', function( event ) {
+ console.log( 'clicked', $( this ).text() );
+});
